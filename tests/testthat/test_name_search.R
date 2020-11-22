3ac313b4c03b6ce2d44da4e8ca98a9800566af48
Markus Skyttner <markussk@kth.se>
2020-11-22 21:58:26 +0100

    update tests for name searching

--- tests/testthat/test_name_search.R
+++ tests/testthat/test_name_search.R
@@ -3,70 +3,69 @@ library(aquamapsdata)
 context("Name Search")
 
 test_that("fuzzy name search works with OR", {
-  with_OR <- am_name_search_fuzzy(search_term = "Cod OR shark")
-  without_OR <- am_name_search_fuzzy(search_term = "Cod")
+  with_OR <- am_search_fuzzy(search_term = "Cod OR shark")
+  without_OR <- am_search_fuzzy(search_term = "Cod")
   expect_gt(nrow(with_OR), nrow(without_OR))
 })
 
 
-test_that("custom query works", {
-  res <- am_sql_query("select key from fts where terms match 'cod'")
-  expect_gt(nrow(res), 0)
-})
+#test_that("custom query works", {
+#  res <- am_sql_query("select key from fts where terms match 'cod'")
+#  expect_gt(nrow(res), 0)
+#})
 
 test_that("exact name search works", {
 
-  expect_gt(nrow(am_name_search_exact()), 0)
+  expect_gt(nrow(am_search_exact()), 0)
 
   expect_gt(
-    nrow(am_name_search_exact(rank_phylum = "Annelida")),
+    nrow(am_search_exact(Phylum = "Annelida")),
     0
   )
 
   expect_gt(
-    nrow(am_name_search_exact(rank_class = "Mammalia")),
+    nrow(am_search_exact(Class = "Mammalia")),
     0
   )
 
   expect_gt(
-    nrow(am_name_search_exact(rank_species = NULL)),
+    nrow(am_search_exact(Species = NULL)),
     0
   )
+
+#  expect_equal(
+#    nrow(am_search_exact(SpeciesID = "Biv-32126")),
+#    1
+#  )
 
   expect_equal(
-    nrow(am_name_search_exact(key = "Biv-32126")),
+    nrow(am_search_exact(Kingdom = NULL)),
     1
   )
 
   expect_equal(
-    nrow(am_name_search_exact(rank_kingdom = NULL)),
-    5
-  )
-
-  expect_equal(
-    nrow(am_name_search_exact(vernacular = "Atlantic cod")),
+    nrow(am_search_exact(FBname = "Atlantic cod")),
     1
   )
 
   expect_gt(
-    nrow(am_name_search_exact(rank_genus = "Alosa")),
-    nrow(am_name_search_exact(rank_genus = "Alosa",
-      rank_species = "fallax"))
+    nrow(am_search_exact(Genus = "Alosa")),
+    nrow(am_search_exact(Genus = "Alosa", Species = "fallax"))
   )
 
 
 })
 
-test_that("exact name search works for binomial names", {
-
-  expect_equal(
-    am_name_search_exact(binomial = "Salmo trutta")$key,
-    "Fis-21630"
-  )
-
-  expect_equal(
-    am_name_search_exact(binomial = "Gadus morhua")$key,
-    "Fis-29394"
-  )
-
-})
+# test_that("exact name search works for binomial names", {
+#
+#   expect_equal(
+#     am_name_search_exact(binomial = "Salmo trutta")$key,
+#     "Fis-21630"
+#   )
+#
+#   expect_equal(
+#     am_name_search_exact(binomial = "Gadus morhua")$key,
+#     "Fis-29394"
+#   )
+#
+# })
