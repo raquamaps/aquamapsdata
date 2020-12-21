1ce236fe1ed968533cfbecb971c448b35d7ac2dc
Markus Skyttner <markussk@kth.se>
2020-12-21 13:52:16 +0100

    fix upload and download (use curl, not download.file) and add IA fcns

--- R/update.R
+++ R/update.R
@@ -5,32 +5,29 @@
 #' download_db()
 #' }
 #' @importFrom R.utils gunzip isGzipped
-#' @importFrom utils download.file
+#' @importFrom curl curl_download
 #' @importFrom rcrypt encrypt decrypt
 #' @export
 #' @family general
 download_db <- function(force = FALSE, passphrase = NULL) {
-
   src <- "https://archive.org/download/aquamapsdb/am.db.gpg"
-  #"http://archive.org/download/aquamapsdata/am.db.gpg"
   temp <- file.path(dirname(tempdir()), "am.db.gpg")
   tgt <- am_db_sqlite()
 
   if (file.exists(tgt) && !force)
     stop("An existing db exists at ", tgt,
-         ", to overwrite, pls rerun with force = TRUE")
+      ", to overwrite, pls rerun with force = TRUE")
 
-  message("Download of aquamapsdb (",
-            src, " -> ", temp, " -> ", tgt, ")")
-
+  message("Download of aquamapsdb (", src, " -> ", temp, " -> ", tgt, ")")
   if (!file.exists(temp)) {
-    download.file(src, temp)
+    dl <- curl::curl_download(src, temp, quiet = FALSE)
   } else {
     message("data appears to have been downloaded already?")
     message("proceeding to extract data ....")
   }
 
   if (!file.exists(tgt)) {
+    if (!dir.exists(dirname(tgt))) dir.create(dirname(tgt), recursive = TRUE)
     message("... decrypting ", temp, " to ", tgt)
     rcrypt::decrypt(temp, passphrase = passphrase, output = tgt)
     message("done")
